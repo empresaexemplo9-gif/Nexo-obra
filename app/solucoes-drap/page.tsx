d652170a54c518fd02340033d1d4fe09d3140aa5
empresaexemplo9-gif <empresaexemplo9@gmail.com>
2026-09-16 03:28:48 -0300

    feat: adiciona página de soluções DRAP na H.OIKOS

---
+++ app/solucoes-drap/page.tsx
@@ -0,0 +1,7 @@
+import { DrapSolutionsWorkspace } from "@/components/drap-solutions-workspace";
+
+export const dynamic = "force-dynamic";
+
+export default function DrapSolutionsPage() {
+  return <DrapSolutionsWorkspace />;
+}
